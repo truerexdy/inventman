3e51d3a97f8d7a75e9bed70d635e58674ec54289
rexdy <jnani@duck.com>
2024-11-15 19:53:45 +0530

    v1 backend

---
+++ src/main/java/org/rexdy/App.java
@@ -0,0 +1,8 @@
+package org.rexdy;
+import org.springframework.boot.SpringApplication;
+
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductController.class, args);
+    }
+}
---
+++ src/main/java/org/rexdy/json.java
@@ -0,0 +1,8 @@
+package org.rexdy;
+
+public class json {
+    int id;
+    String unitname;
+    int units;
+    double unitprice;
+}
